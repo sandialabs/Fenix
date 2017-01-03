379f36d886d987da15665eddc4e29bb5b9483171
ericvalens <evalen@sandia.gov>
2017-01-03 16:39:29 -0700

    update examples

---
+++ examples/03_reduce/mpi/CMakeFiles/mpi_reduce.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mpi_reduce.dir/mpi_reduce.c.o"
+  "mpi_reduce.pdb"
+  "mpi_reduce"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mpi_reduce.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
