0910f9e988df5f714ed05aebda1e0cfe20845716
ericvalens <evalen@sandia.gov>
2017-01-03 17:00:47 -0700

    remove cmake auto gen files

--- examples/03_reduce/mpi/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo number of process = 4, msg = 7
-/home/evalen/devtools/bin/mpirun -n 4 ./mpi_ring 7
-
-echo number of process = 7, msg = 4
-/home/evalen/devtools/bin/mpirun -n 7 ./mpi_ring 4
