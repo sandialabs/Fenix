674acbe15b8333fbfabf8e3be14a6664b0841c4c
Teranishi <knteran@mag-client14.ca.sandia.gov>
2017-02-12 03:02:48 -0800

    Debugging

--- include/fenix_data_recovery_ext.h
+++ include/fenix_data_recovery_ext.h
@@ -58,6 +58,6 @@
 #define __FENIX_EXT_DATA_RECOVERY_H__
 #include "fenix_data_recovery.h"
 
-extern fenix_group_t *__fenix_g_data_recovery;
+//extern fenix_group_t *__fenix_g_data_recovery;
 #endif // FENIX_EXT_DATA_RECOVERY_H
 
