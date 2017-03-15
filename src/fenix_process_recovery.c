66f31c680559ee13b7a3f882d7d7541f7892ffac
rfvander <rob.f.van.der.wijngaart@intel.com>
2017-03-15 16:57:21 -0700

    Removing duplicate definitions of MPI_Comm_size/rank, and added missing spare_rank declaration in fenix_process_recovery.c (Barrier code).

--- src/fenix_process_recovery.c
+++ src/fenix_process_recovery.c
@@ -753,7 +753,7 @@ void __fenix_finalize() {
 
 
   if (__fenix_get_current_rank(*__fenix_g_world) == 0) {
-   // int spare_rank = __fenix_get_world_size(*__fenix_g_world) - 1;
+    int spare_rank;
     PMPI_Comm_size(*__fenix_g_world, &spare_rank);
     spare_rank--;
     int a;
