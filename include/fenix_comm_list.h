7fc2bea90f4c0d9ebaaf03c494c8359679cfd11e
rfvander <rob.f.van.der.wijngaart@intel.com>
2017-07-07 22:44:20 -0700

    Adding fenix_comm_list.h

--- include/fenix_comm_list.h
+++ include/fenix_comm_list.h
@@ -53,85 +53,15 @@
 // ************************************************************************
 //@HEADER
 */
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <fenix.h>
-#include <fenix_process_recovery.h>
+#ifndef __FENIX_COMM_LIST_H__
+#define __FENIX_COMM_LIST_H__
  
-fenix_comm_list_t my_list = {NULL, NULL};
-
-int __fenix_comm_push(MPI_Comm *comm) {
-  fenix_comm_list_elm_t *current = (fenix_comm_list_elm_t *) malloc(sizeof(fenix_comm_list_elm_t));
-  if (!current) return 0;
-  current->next = NULL;
-  current->comm = comm;
-  if (!my_list.tail) {
-    /* if list was empty, initialize head and tail                             */
-    current->prev = NULL;
-    my_list.head = my_list.tail = current;
-  }
-  else {
-    /* if list was not empty, add element to the head of the list              */
-    current->prev = my_list.head;
-    my_list.head->next = current;
-    my_list.head = current;
-  }
-  return FENIX_SUCCESS;
-}
-
-int __fenix_comm_delete(MPI_Comm *comm) {
-
-  fenix_comm_list_elm_t *current = my_list.tail;
-  while (current) {
-    if (*(current->comm) == *comm) {
-      if (current != my_list.head && current != my_list.tail) {
-	current->prev->next = current->next;
-        current->next->prev = current->prev;
-      }
-      else if (current == my_list.tail) {
-        if (current->next) {
-          current->next->prev = NULL;
-          my_list.tail = current->next;
-	}
-        else my_list.tail = my_list.head = NULL;
-      }
-      else {
-        if (current->prev) {
-          current->prev->next = NULL;
-          my_list.head = current->prev;
-	}
-        else my_list.tail = my_list.head = NULL;
-      }
-      MPIF_Comm_revoke(*comm);
-      PMPI_Comm_free(comm);
-      free(current);
-      return 1;
-    }
-    else current = current->next;
-  }
-  /* if we end up here, the requested communicator has not been found */
-  return 0;
-}
-  
-
-void __fenix_comm_list_destroy(void) {
-  if (my_list.tail == NULL) {
-    return;
-  }
-  else {
-    fenix_comm_list_elm_t *current = my_list.tail;
-    while (current->next) {
-      fenix_comm_list_elm_t *new = current->next;
-      MPIF_Comm_revoke(*current->comm);
-      PMPI_Comm_free(current->comm);
-      free(current);
-      current = new;
-    }
-    MPIF_Comm_revoke(*current->comm);
-    PMPI_Comm_free(current->comm);
-    free(current);
-  }
-  my_list.tail = my_list.head = NULL;
-}
-
+#include <mpi.h>
+ 
+int __fenix_comm_push(MPI_Comm *comm);
+ 
+int __fenix_comm_delete(MPI_Comm *comm);
+ 
+void __fenix_comm_list_destroy(void);
+ 
+#endif // FENIX_COMM_LIST_H
