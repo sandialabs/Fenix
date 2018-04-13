9b1f922a1fd15ee7770225e5d72ec10e28ee5e28
Marc Gamell <marc.gamell.balmana@intel.com>
2018-04-13 17:19:28 -0500

    a

--- src/fenix_callbacks.c
+++ src/fenix_callbacks.c
@@ -57,7 +57,6 @@
 #include <assert.h>
 #include "fenix_constants.h"
 #include "fenix_comm_list.h"
-#include "fenix_process_recovery_global.h"
 #include "fenix_process_recovery.h"
 #include "fenix_data_group.h"
 #include "fenix_data_recovery.h"
