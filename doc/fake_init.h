e0ce1f88391b3c8a1749fcce6635e4b4b5695f96
Matthew Whitlock <matthew@mwhitlo.dev>
2024-10-03 16:08:10 -0400

    Initial doxygen setup
    Documentation
    
    Organize, setup versioned docs
    
    Deploy docs to Github pages
    
    Update doxygen header
    
    Use Doxygen 1.12.0 for github pages builds
    
    Improved Process Recovery docs writeup
    
    'make doc' -> 'make docs'

---
+++ doc/fake_init.h
@@ -0,0 +1,4 @@
+//!@weakgroup ProcessRecovery
+//!@{
+void Fenix_Init(int* role, MPI_Comm comm, MPI_Comm* newcomm, int** argc, char*** argv, int spare_ranks, int spawn, MPI_Info info, int* error);
+//!@}
